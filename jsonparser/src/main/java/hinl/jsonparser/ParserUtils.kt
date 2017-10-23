564489dd446bea79f9b2c10cc70ae7700c35651b
tszhin.lai <tszhin>
2017-10-23 12:44:29 +0800

    -Initialize project

---
+++ jsonparser/src/main/java/hinl/jsonparser/ParserUtils.kt
@@ -0,0 +1,16 @@
+package hinl.jsonparser
+
+import org.json.JSONObject
+import java.util.HashMap
+import kotlin.reflect.KClass
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.javaField
+
+
+@Target (AnnotationTarget.FIELD)
+@Retention (AnnotationRetention.RUNTIME)
+annotation class Schema(val JsonName: String = "",
+                        val Serializable: Boolean = true,
+                        val DeSerializable: Boolean = false)
