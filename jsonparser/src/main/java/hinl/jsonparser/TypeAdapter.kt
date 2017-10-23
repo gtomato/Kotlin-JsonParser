0e04d834ea3f8733a50256cd05a70e345ad675e8
Yu Wing Ng <wingyng26@gmail.com>
2017-10-23 17:02:26 +0800

    Update type adapter

--- jsonparser/src/main/java/hinl/jsonparser/TypeAdapter.kt
+++ jsonparser/src/main/java/hinl/jsonparser/TypeAdapter.kt
@@ -8,7 +8,7 @@ import java.io.IOException
 
 abstract class TypeAdapter<T> {
 
-    abstract fun write(output: JSONObject, value: T?): JSONObject
+    abstract fun write(output: JSONObject, key: String, value: T?): JSONObject
 
     abstract fun read(input: JSONObject, key: String): T?
 }
