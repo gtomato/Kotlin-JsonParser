6f3c987f1587ccf43e8767c78d0e94e19f2d8ca5
tszhin.lai <tszhin>
2017-10-25 12:40:29 +0800

    -Add --Default Base TypeAdapterMap

--- jsonparser/src/main/java/hinl/jsonparser/JsonFormatter.kt
+++ jsonparser/src/main/java/hinl/jsonparser/JsonFormatter.kt
@@ -1,6 +1,44 @@
 package hinl.jsonparser
 
+import android.util.Log
+import hinl.jsonparser.typeadapter.*
+import org.json.JSONArray
+import org.json.JSONObject
+import java.math.BigDecimal
+import kotlin.reflect.KClass
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.jvmErasure
 
-internal class JsonFormatter {
-    
+
+class JsonFormatter(
+        typeAdapterMap: HashMap<KClass<*>, TypeAdapter<*>>? = null,
+        dateFormat: String = DAFAULT_DATE_FORMAT
+
+) {
+
+    companion object {
+        val DEFAULT_TypeAdapterMap = hashMapOf<KClass<*>, TypeAdapter<*>>(
+                Boolean::class to BooleanTypeAdapter(),
+                Char::class to CharSequenceTypeAdapter(),
+                String::class to StringTypeAdapter(),
+                Char::class to CharTypeAdapter(),
+                Int::class to IntTypeAdapter(),
+                Long::class to LongTypeAdapter(),
+                Short::class to ShortTypeAdapter(),
+                Double::class to DoubleTypeAdapter(),
+                Float::class to FloatTypeAdapter(),
+                BigDecimal::class to BigDecimalTypeAdapter()
+        )
+
+        val DAFAULT_DATE_FORMAT = ""//TODO("DateFormate")
+    }
+
+    val mTypeAdapterMap = mutableMapOf<KClass<*>, TypeAdapter<*>>().apply {
+        putAll(DEFAULT_TypeAdapterMap)
+        typeAdapterMap?.let {
+            putAll(it)
+        }
+    }
+    val mDateFormat: String = dateFormat
+
 }
