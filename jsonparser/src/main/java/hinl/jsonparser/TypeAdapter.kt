2fb513596130154afe75eb9d47d2003357cf7002
Yu Wing Ng <wingyng26@gmail.com>
2017-10-25 11:37:53 +0800

    Update to add get Type adapter map

--- jsonparser/src/main/java/hinl/jsonparser/TypeAdapter.kt
+++ jsonparser/src/main/java/hinl/jsonparser/TypeAdapter.kt
@@ -5,8 +5,12 @@ import android.util.JsonWriter
 import org.json.JSONObject
 
 import java.io.IOException
+import kotlin.reflect.KClass
 
 abstract class TypeAdapter<T> {
+    protected fun getTypeAdapterMap(): Map<KClass<*>, TypeAdapter<*>> {
+        TODO("refer to the static ref of map")
+    }
 
     abstract fun write(output: JSONObject, key: String, value: T?): JSONObject
 
